f8f3921c93c6bd57505cd85aa335d6345bd5ecea
unknown <pvelasquez@umindanao.edu.ph>
2021-05-07 18:35:45 +0800

    version 4

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
- header("Location:../excel_reader/src/test.php");
-
-?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6d618f569113d76d226798bf805485aff7e7efde',
+    'name' => 'shuchkin/simplexlsx',
+  ),
+  'versions' => 
+  array (
+    'shuchkin/simplexlsx' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d618f569113d76d226798bf805485aff7e7efde',
+    ),
+  ),
+);
